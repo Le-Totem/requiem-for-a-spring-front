b21efafda3b16ce86b5bd57d66219c284ff8506c
Nimwe <floallheilly@gmail.com>
2025-09-23 11:45:16 +0200

    Composants React Portée, Note et Entête.

---
+++ composants/partition/src/App.tsx
@@ -0,0 +1,13 @@
+
+import Partition from './Partition';
+
+function App() {
+  return (
+    <div style={{ padding: '10px' }}>
+      <h2>Portée musicale responsive</h2>
+      <Partition />
+    </div>
+  );
+}
+
+export default App;
