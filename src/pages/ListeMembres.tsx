e2d75703b10582cd1d86c070d8d448fabf0a5dc4
Nimwe <floallheilly@gmail.com>
2025-10-20 14:22:31 +0200

    Front de la page Liste Membres avec renvoi de console.log pour le Crud

--- src/pages/ListeMembres.tsx
+++ src/pages/ListeMembres.tsx
@@ -1,68 +1,90 @@
 import { Note } from "../components/pathButtons/Note";
 import Partition from "../components/Partition";
 import TitlePartition from "../components/TitlePartition";
+import "../styles/listemembres.css";
 
-export default function ListeMembres() {
+export default function listemembres() {
     return (
-        <main className="listeMembres-container">
+        <main className="inscription-container">
             <div className="title-wrapper">
                 <TitlePartition text="Liste des Membres" />
             </div>
 
             <div className="crud">
-                <div className="partitionCrud">
-                    <Partition />
+                <div className="partition-wrapper">
+                    <div className="notes-on-staff">
+                        <button className="bouton-note" onClick={() => console.log("Invitation envoyée")}>
+                            <Note
+                                x={0}
+                                y={0}
+                                xtext={15}
+                                label="Inviter"
+                                iconType="blanche"
+                                isOnStaff={false}
+                                size={5}
+                            />
+                        </button>
 
-                    <button className="crud-button">
-                        <Note
-                            x={0}
-                            y={0}
-                            xtext={15}
-                            label="Inviter"
-                            iconType="blanche"
-                            isOnStaff={false}
-                        />
-                    </button>
+                        <button className="bouton-note" onClick={() => console.log("Mise à jour ok")}>
+                            <Note
+                                x={0}
+                                y={0}
+                                xtext={15}
+                                label="Mettre à jour"
+                                iconType="blanche"
+                                isOnStaff={false}
+                                size={5}
+                            />
+                        </button>
 
-                    <button className="crud-button">
-                        <Note
-                            x={0}
-                            y={0}
-                            xtext={15}
-                            label="Mettre à jour"
-                            iconType="blanche"
-                            isOnStaff={false}
-                        />
-                    </button>
+                        <button className="bouton-note" onClick={() => console.log("Suppression ok")}>
+                            <Note
+                                x={0}
+                                y={0}
+                                xtext={20}
+                                label="Supprimer"
+                                iconType="doubleSharp"
+                                isOnStaff={false}
+                                size={5}
+                            />
+                        </button>
+                    </div>
                 </div>
             </div>
 
-            <div className="listeMembres-container">
-                <div className="partition-wrapper">
-                    <span className="labelMembres gauche">
-                        <Note
-                            x={0}
-                            y={0}
-                            xtext={0}
-                            label=""
-                            iconType="croche"
-                            isOnStaff={false}
-                        />
-                        Nom du membre
-                    </span>
-                    <button className="labelMembres droite">
-                        <Note
-                            x={0}
-                            y={0}
-                            xtext={0}
-                            label=""
-                            iconType="croche"
-                            isOnStaff={false}
-                        />
-                        Supprimer
-                    </button>
-                    <Partition />
-                </div>
+
+            <div className="partitions-container">
+                {[1, 2, 3, 4, 5].map((i) => (
+                    <div className="partition-wrapper" key={i}>
+                        <span className="labelMembres gauche">
+                            <Note
+                                x={0}
+                                y={0}
+                                xtext={0}
+                                label=""
+                                iconType="croche"
+                                isOnStaff={false}
+                                size={10}
+                            />
+                            Personne {i}
+                        </span>
+
+                        <span className="labelMembres droite">
+                            <Note
+                                x={0}
+                                y={0}
+                                xtext={0}
+                                label=""
+                                iconType="croche"
+                                isOnStaff={false}
+                                size={10}
+                            />
+                            Personne {i + 5}
+                        </span>
+
+                        <Partition />
+                    </div>
+                ))}
             </div>
         </main>
     );
